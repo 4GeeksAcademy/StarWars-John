7a254701574736e3bb0a71badc2eaff897176c5d
johnocamposabg-dev <john.ocamposabg@gmail.com>
2026-01-06 00:21:51 +0000

    inicio

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,13 +2,16 @@ import { Outlet } from "react-router-dom/dist"
 import ScrollToTop from "../components/ScrollToTop"
 import { Navbar } from "../components/Navbar"
 import { Footer } from "../components/Footer"
+import { Card } from "../components/cards"
 
 // Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
     return (
         <ScrollToTop>
             <Navbar />
+                <Card />
                 <Outlet />
+                
             <Footer />
         </ScrollToTop>
     )
